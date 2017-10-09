11899347e75e93617ac6894ec797cb92c2902e14
강희명 <hm@ganghuimyeongs-MacBook-Pro.local>
2017-10-09 15:10:11 +0900

    change homemaster tag manager

--- js/query.js
+++ js/query.js
@@ -2,7 +2,7 @@
 server host  : https://homemaster-service.com*/
 //var getAPIURL = "http://192.168.0.114:8888";
 //var getAPIURL = "https://homemaster-service.com";
-var getAPIURL ="103.3.61.190:8888"; 
+var getAPIURL = "https://103.3.61.190:8888";
 /*
 $.ajax(function(){
 	method:"POST",
